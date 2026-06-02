86c8f1865729df010f3bf02a2a05e55143cbd6b8
Navz <mnavarroze@sv-SlgBQ2IFfLn8N6zRM1Oi.cloud.elastika.pe>
2026-06-02 09:49:46 -0500

    changed vps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    disableStaticImages: false,
+  },
 };
 
 export default nextConfig;
