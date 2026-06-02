e32b0e57d6e07a6fbea61953165a6e71adf50ffd
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2026-06-02 03:42:21 -0500

    persistnecia de imagenes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    unoptimized: true, // Desactiva optimización de imágenes
+  },
 };
 
 export default nextConfig;
